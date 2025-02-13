295f1ca5dcbda178995661f47a0a95915c9e15ef
useryesa9@gmail.com <useryesa9@gmail.com>
2025-02-13 21:37:12 +0700

    multiple interface

--- main.go
+++ main.go
@@ -6,10 +6,21 @@ type Makhluk interface {
 	bernafas() string
 }
 
+type Berjalan interface {
+	berjalan() string
+}
+
+type Berenang interface {
+	berenang() string
+}
+
 type Manusia struct{}
 
 func (m Manusia) bernafas() string {
 	return "Manusia Bernafas!"
+}
+func (m Manusia) berjalan() string {
+	return "Manusia Berjalan!"
 }
 
 type Hewan struct{}
@@ -17,9 +28,20 @@ type Hewan struct{}
 func (h Hewan) bernafas() string {
 	return "Hewan Bernafas!"
 }
+func (h Hewan) berenang() string {
+	return "Hewan Berenang"
+}
 
 func isBernafas(m Makhluk) {
 	fmt.Println(m.bernafas())
+}
+
+func isBerjalan(m Berjalan) {
+	fmt.Println(m.berjalan())
+}
+
+func isBerenang(m Berenang) {
+	fmt.Println(m.berenang())
 }
 
 func main() {
@@ -27,5 +49,8 @@ func main() {
 	var hewan Hewan
 
 	isBernafas(manusia)
+	isBerjalan(manusia)
+	fmt.Println("")
 	isBernafas(hewan)
+	isBerenang(hewan)
 }
